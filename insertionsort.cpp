6dee04c014eac52e5ecaacd247c6f65f78e15a12
Haffi24 <haffisaefulloh@gmail.com>
2025-03-26 23:08:04 +0700

    menambahkan variable Data TEMP

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -32,3 +32,22 @@ void input() //menambahkan perulangan while
         cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
     }
 }
+
+void insertionSort()
+{ // Procedure Insertionsort
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j, i; // Membuat variable j sebagai penanda
+
+    for (i = 1; i < n; i++)
+    { // Step 1
+        temp = arr[i]; // Step 2
+        j = i - 1; // Step 3
+
+        while (j >= 0 && arr[j] > temp) // Step 4
+        {
+            arr[j + 1] = arr[j]; // Step 4a
+            j--; // Step 4b
+        }
+        arr[j + 1] = temp; // Step 5
+    }
+}
