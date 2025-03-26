2ca5299cb9b53910ae7d76db1ddc2294aad40238
Haffi24 <haffisaefulloh@gmail.com>
2025-03-26 23:09:06 +0700

    menambahkan output

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -51,3 +51,15 @@ void insertionSort()
         arr[j + 1] = temp; // Step 5
     }
 }
+
+void display()
+{
+    cout << endl; // Output baris Kosong
+    cout << "======================" << endl;
+    cout << "Total Passs = " << n - 1 << endl; // count element movement
+    cout << "======================" << endl; // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+    cout << "======================" << endl; // Output ke layar
+
+    
+
